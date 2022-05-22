771d44cf82f87d8e412cba73d316b04e31078f12
Farin Dian Nugraha <farindn@gmail.com>
2022-05-22 07:32:10 +0700

    update release

--- build/app/generated/source/buildConfig/release/com/example/flutter_room_finder_app/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/example/flutter_room_finder_app/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.flutter_room_finder_app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.example.flutter_room_finder_app";
+  public static final String APPLICATION_ID = "com.farindn.roomfinder";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
